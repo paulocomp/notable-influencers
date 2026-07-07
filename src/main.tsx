5e070f7b8ddde9cfe61954fa4fc75ca0ccc8361a
paulocomp <paulocorreacomp@gmail.com>
2026-07-07 18:56:32 -0300

    chore: conserta pequenas inconsistências

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import GlobeView from "./feat/globe/component/GlobeView.tsx";
+import App from "./app/App.tsx";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <GlobeView/>
+        <App/>
     </StrictMode>,
 )
